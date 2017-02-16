98264ba25db239bcc0fa483c9b76dd52d0647839
Quentin Santos <quentin.santos@worldonline.fr>
2017-02-16 19:21:25 +0100

    Make singen much faster

--- singen.c
+++ singen.c
@@ -1,7 +1,6 @@
-#define _DEFAULT_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <unistd.h>
 
 unsigned char float2byte(float x) {
     /* Map float range [-1, 1] to integer range [0, 255] */
@@ -12,16 +11,24 @@ unsigned char float2byte(float x) {
     return (unsigned char) r;
 }
 
-int main() {
-    unsigned long long i = 0;
-    while (1) {
-        float x = (float) i * .01f;
+int main(int argc, char** argv) {
+    unsigned int period = 100000;
+    unsigned char* points = malloc(period);
+    if (points == NULL) {
+        fprintf(stderr, "could not allocate enough memory for `points`");
+        return 1;
+    }
+    for (unsigned int i = 0; i < 1000; i += 1) {
+        float x = (float) i / (float) period * 6.283185307179586f;
         unsigned char value = float2byte(sinf(x));
-        fputc(value, stdout);
-        fflush(stdout);
-        usleep(1000);
-        i += 1;
+        points[i] = value;
     }
 
+    while (1) {
+        fwrite(points, 1, period, stdout);
+        fflush(stdout);
+    }
+
+    free(points);
     return 0;
 }
